e9dc8e10982aec33622b7668dfa8d7fa4901de1c
CJ <hlopin.ig@yandex.ru>
2018-06-17 18:53:06 +0300

    1

--- task1.rb
+++ task1.rb
@@ -2,22 +2,14 @@ i = ARGV[0]
 a = ARGV[1]
 b = ARGV[2]
 
-def changer(i ,a ,b)
+def changer(i ,a)
   i.to_s
-  case a 
-    when 2
-      return 0bi
-    when 8 
-      return 0oi
-    when 16
-      return oxi
-  end 
+  return Integer(i, a)
 end
 
 if a != 10 
-  i = changer(i, a, b)
+  i = changer(i, a)
   a = 10
-  puts i
 end
   
 res = i.to_s(b)
